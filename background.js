cb5d44ebeec6ec0300c5e41ad55ea3ca622a2a1b
Amir Sarsenov <invisible088@gmail.com>
2021-12-02 11:02:53 +0100

    release v1

--- background.js
+++ background.js
@@ -1,11 +1,13 @@
-let state = false;
-
-chrome.browserAction.onClicked.addListener(function (tab) {
-  if (!state) {
+chrome.webNavigation.onCompleted.addListener(
+  function () {
     chrome.tabs.insertCSS(null, { file: "dark_mode.css" });
-    state = !state;
-  } else {
-    chrome.tabs.removeCSS(null, { file: "dark_mode.css" });
-    state = !state;
+    chrome.tabs.executeScript(null, { file: "script.js" });
+  },
+  {
+    url: [
+      {
+        hostContains: "my.epitech.eu",
+      },
+    ],
   }
-});
+);
